333862f265d0c5d808e37c3118d3749239add228
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2026-01-01 16:31:04 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 
 
-// function btn1() {
-//     document.getElementById("one").style.color = "blue"
-// }
+function btn1() {
+    document.getElementById("one").style.color = "blue"
+}
 
-// function btn2() {
-//     document.getElementById("two").style.color = "green"
-// }
+function btn2() {
+    document.getElementById("two").style.color = "green"
+}
 
-// function btn3() {
-//     document.getElementById("three").style.color = "darkcyan"
-// }
+function btn3() {
+    document.getElementById("three").style.color = "darkcyan"
+}
 
-// function btn4() {
-//     document.getElementById("four").style.color = "cornflowerblue"
-// }
+function btn4() {
+    document.getElementById("four").style.color = "cornflowerblue"
+}
 
-// function btn5() {
-//     document.getElementById("five").style.color = "grey"
-// }
+function btn5() {
+    document.getElementById("five").style.color = "grey"
+}
 
-// function btn6() {
-//     document.getElementById("six").style.color = "slateblue"
-// }
+function btn6() {
+    document.getElementById("six").style.color = "slateblue"
+}
 
-// function btnReset() {
-//     document.getElementById("one").style.color = "black"
-//     document.getElementById("two").style.color = "black"
-//     document.getElementById("three").style.color = "black"
-//     document.getElementById("four").style.color = "black"
-//     document.getElementById("five").style.color = "black"
-//     document.getElementById("six").style.color = "black"
-// }
+function btnReset() {
+    document.getElementById("one").style.color = "black"
+    document.getElementById("two").style.color = "black"
+    document.getElementById("three").style.color = "black"
+    document.getElementById("four").style.color = "black"
+    document.getElementById("five").style.color = "black"
+    document.getElementById("six").style.color = "black"
+}
